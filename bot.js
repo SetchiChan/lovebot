30d1f7c79ac4ad14b8ad91c146bc898a4b2dbd3a
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2018-11-30 16:08:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,23 +10,8 @@ bot.on('message', async message => {
     const vartom = message.guild.members.random();
     const varnom = message.guild.members.random();
 
-    const ship1 = message.guild.members.random();
-    const ship2 = message.guild.members.random();
-    const ship3 = message.guild.members.random();
-
-    var rademor = getRandomInt(101)
-    var rademorr = 200 - rademor
-
-    const rando_imgs = [
-        'youtube.com',
-        ]
-
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
-    };
-
-    if (message.content.startsWith("!ship")) {
-        message.channel.send("Congratulations to our new couple! " + vartom + " and " + varnom + " are a beautiful couple!");
+    if (message.content.startsWith("!mistletoe")) {
+        message.channel.send("↑ \n| \nThe person above me and the person below me now have to kiss! \n| \n↓");
     }
 
 
