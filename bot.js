98ca1e7a31203b93d878ed71770a391e5b8636f7
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2019-03-05 17:49:43 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,74 +12,78 @@ bot.on('message', async message => {
     const varhom = message.guild.members.random();
 
     //Names
-    if (message.content.startsWith("!threeway")) {
-        message.channel.send(vartom + "'s fantasy harem involves " + varnom + " and " + varhom + ".");
+    if (message.content.startsWith("!bunny")) {
+        message.channel.send("The Easter Bunny made " + vartom + " and " + varnom + " fall in love.");
     }
     
     if (message.content.startsWith("!mercedes")) {
-        message.channel.send("Mercedes found somebody to love and it was " + vartom + ".");
+        message.channel.send("Mercedes opened the easter egg and found a note within it. It read that she will fall in love with " + vartom + ".");
     }
 
     if (message.content.startsWith("!ethan")) {
-        message.channel.send("Ethan will be eternally forever alone. Contact him at @setchichan if you need a fourth wheel.");
+        message.channel.send("Even though my birthday is on April, everyone will forget it anyways. Also nothing new here.");
     }
 
     if (message.content.startsWith("!emily")) {
-        message.channel.send("Emily cought " + vartom + " writing her a love letter.");
+        message.channel.send("Emily stairs into " + vartom + " eyes, then throws the easter bunny at them!");
     }
 
     if (message.content.startsWith("!olivia")) {
-        message.channel.send("Olivia takes out the gag ball. She wants to put it on " + vartom + ".");
+        message.channel.send("Olivia did a threeway with the easter bunny and " + vartom + ".");
     }
 
     if (message.content.startsWith("!daniil")) {
-        message.channel.send("Daniil confessed to " + vartom + " and died.");
+        message.channel.send("Daniil killed the easter bunny and gave the corpse to " + vartom + ".");
     }
 
     if (message.content.startsWith("!merissa")) {
-        message.channel.send("Merissa sent something kinky to " + vartom + ".");
+        message.channel.send("Merissa sent the lewd bunny to " + vartom + ".");
     }
 
     if (message.content.startsWith("!brian")) {
-        message.channel.send("Brian found (generic love message) and loved " + vartom + ".");
+        message.channel.send("Brian x " + vartom + ". (I was lazy to give you one brian)");
+    }
+
+    if (message.content.startsWith("!dylan")) {
+        message.channel.send("Dylan-sama gave their eggs to " + vartom + " and they ate them.");
     }
 
     //General Commands
-    if (message.content.startsWith("!propose")) {
-        message.reply(" and proposed to " + vartom + " and took their hand in marraige.");
+    if (message.content.startsWith("!easterkink")) {
+        message.reply(" wants to have " + vartom + " eggs if you know what I mean.");
     }
 
-    if (message.content.startsWith("!valentines")) {
-        message.channel.send(vartom + " got a kiss from " + varnom + ". How sweet <3~");
+    if (message.content.startsWith("!easterkinkwitholivia")) {
+        message.reply(" wants to have <@408822490022805504>'s juicy eggs");
     }
 
-    if (message.content.startsWith("!naughty")) {
-        message.channel.send(vartom + " and " + varnom + " were doing something **naughty** under the light of the moon. :rose: ");
+    if (message.content.startsWith("!easterhunt")) {
+        message.reply(" spent their time with " + varnom + " on a easter night~. Ew love.");
     }
 
-    if (message.content.startsWith("!lonely")) {
-        message.channel.send("Don't worry... " + vartom + " is thinking about you.");
+    if (message.content.startsWith("!naughtyeaster")) {
+        message.channel.send(vartom + " had " + varnom + "'s chocolate bunny if you know what I mean. *wink* ");
     }
 
     //Mentions
     if (message.content.startsWith("mercedes")) {
-        message.channel.send("<@196593261828243456> got annoyed you pinged her.");
+        message.channel.send("<@196593261828243456> got egged.");
     }
 
     if (message.content.startsWith("ethan")) {
-        message.channel.send("<@416429218960769026> wants to ruin your relationship.");
+        message.channel.send("<@416429218960769026> is always forgotten.");
     }
 
     if (message.content.startsWith("marissa")) {
-        message.channel.send("<@268519886328692737> lewded everything on this discord.");
+        message.channel.send("<@268519886328692737> lewded the easter bunny. oh gOD. NOO NOT THE R34.");
     }
 
     if (message.content.startsWith("death")) {
-        message.channel.send("<@297867268161798144> is gagging the children.");
+        message.channel.send("<@297867268161798144> is gagging the easter bunny.");
     }
 
     if (message.content.startsWith("olivia")) {
-        message.channel.send("ONI-CHAN!~");
+        message.channel.send("Bunny-Sama~ give me your big juicy egg-sama~");
     }
     
     if (message.content.startsWith("asdasdasdasd")) {
@@ -102,7 +106,7 @@ bot.on('message', async message => {
 });     
 
 bot.on('ready', () => {
-    bot.user.setGame('Hentai Love')
+    bot.user.setGame('Laying the Easter Love')
 })
 
 
