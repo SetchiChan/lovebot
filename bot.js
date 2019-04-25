5adedc3da612b18a44daf946e1531dff7dfbdb32
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2019-04-25 03:29:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,27 +11,20 @@ bot.on('message', async message => {
     const varnom = message.guild.members.random();
     const varhom = message.guild.members.random();
 
-    if (message.content.startsWith("Duolingo" || "duolingo")) {
-        message.channel.send("Have you learned your spanish?... *Holds a bat ready*");
-    }
-
     //Easter Event
 
     const Ethan = '416429218960769026';
-    const Mercedes = '';
-    const Daniel = '';
-    const Olivia = ''
-    const Dylan = '';
-    const Marissa ='';
+    const Mercedes = '196593261828243456';
+    const Daniel = '408822490022805504';
+    const Olivia = '408822490022805504';
+    const Dylan = '494974507283513362';
+    const Marissa ='268519886328692737';
 
     if(message.content.startsWith("!Status")){
         message.channel.send({embed: {
             color: 0x0099ff,
             title: 'Status of All Characters',
             description: 'You can see who is alive or dead here!',
-            thumbnail: {
-                url: 'https://images.pexels.com/photos/953214/pexels-photo-953214.jpeg?cs=srgb&dl=backgrounds-blank-blue-953214.jpg&fm=jpg',
-            },
             fields: [
                 {
                     name: 'Earth-Chan',
@@ -58,20 +51,6 @@ bot.on('message', async message => {
         });     
     }
     
-    //Earth-chan
-    if(message.content.startsWith("!lovekun ship")){
-        message.channel.send({embed: {
-            author: {
-                name: "Love-Kun",
-                icon_url: "https://pm1.narvii.com/6739/5589806a476247d5bd3052a4690bc19d176373cev2_hq.jpg"
-            },
-            description: "Hey there love birds~ " + vartom + " and " + varnom + " are perfect together. <3",
-            color: 44678,
-            fields: []
-            }
-       })
-    }
-
     //Earth-Chan
     if (message.isMentioned(bot.user)) {
         message.reply({embed: {
@@ -80,90 +59,118 @@ bot.on('message', async message => {
                 icon_url: "https://vignette.wikia.nocookie.net/virtualyoutuber/images/f/f2/EarthChan.jpg/revision/latest?cb=20180527004331"
             },
             description: "Yes? Oh! Please use !command to see the available commands!",
-            color: 44678,
+            color: 59431,
             fields: []
             }
        })
     }
     
     if(message.content.startsWith("!command")){
-        message.channel.send({embed: {
+        message.reply({embed: {
+            color: 59431,
             author: {
                 name: "Earth-Chan",
                 icon_url: "https://vignette.wikia.nocookie.net/virtualyoutuber/images/f/f2/EarthChan.jpg/revision/latest?cb=20180527004331"
             },
-            description: "Oh sorry... but Ethan didn't fix this yet. My bad.",
-            fields: []
-            }
-       })
+            description: 'Oh hey human! I see youre new here to this universe! Let me introduce you some of my friends!',
+            fields: [
+                {
+                    name: 'Love-Kun!',
+                    value: 'He is pretty weird. He tends to be very perverted at times. Baka baka. Anyways, you can check out his command set by doing "!lovekun help"',
+                },
+                {
+                    name: 'Chrome-Chan!',
+                    value: 'Chrome-Chan is very smart. She knows like almost everything. She does... seem pretty mean at times... but you might get to like her. Use "!chromechan help" for her command set.',
+                },
+                {
+                    name: 'Duolingo-Chan!',
+                    value: 'Duolingo-Chan is very persistant in me learning Spaceanese. I have too afterall... She will take Moon-Kun from me... Use "!duolingochan help" to check out her command set!',
+                },
+                {
+                    name: 'And Finally... Me! Earth-Chan!',
+                    value: 'Hey human! You live on me! Ill show you this universe and Ill try my best to guide you! Use "!earthchan help" to get my command list!',
+                },
+                {
+                    name: 'Oh yeah, some general commands too!',
+                    value: '"!command" shows available bot commands. "!status" shows the current status of all the bots here!',
+                },
+                {
+                    name: '(PLEASE TYPE ALL COMMANDS IN LOWERCASE FORM SO IT WILL WORK)',
+                    value: '.',
+                },
+            ],
+        }
+        }); 
     }
 
-    var lovemeter = 5;
-
-    if(message.content.startsWith("!earthchan flirt")){
-        message.channel.send({embed: {
+    if (message.content.startsWith("!earthchan help")) {
+        message.reply({embed: {
+            color: 59431,
             author: {
                 name: "Earth-Chan",
                 icon_url: "https://vignette.wikia.nocookie.net/virtualyoutuber/images/f/f2/EarthChan.jpg/revision/latest?cb=20180527004331"
             },
-            description: "(Planning for this command to be random) Oh my... Please use '!earthchan yes' to say earth-chan is flat or '!earthchan no' to say earth-chan isn't flat.",
-            image: {
-                url: "https://i.ytimg.com/vi/0wQ5bFRoJe0/maxresdefault.jpg"
-            },
-            fields: []
-            }
-       })
+            description: 'Hey I see you want to explore this world! Here are my commands!',
+            fields: [
+                {
+                    name: '!flirt earthchan [BROKEN/WIP]',
+                    value: 'But Im a planet human... I dont think you can do that...',
+                },
+                {
+                    name: 'MORE COMMANDS COMING SOON!',
+                    value: '.',
+                },
+            ],
+        }
+        }); 
     }
-
-    if(message.content.startsWith("!earthchan flirt")){
-        message.channel.send({embed: {
-            author: {
-                name: "Earth-Chan",
-                icon_url: "https://vignette.wikia.nocookie.net/virtualyoutuber/images/f/f2/EarthChan.jpg/revision/latest?cb=20180527004331"
-            },
-            description: "(Planning for this command to be random) Oh my... Please use '!earthchan yes' to say earth-chan is flat or '!earthchan no' to say earth-chan isn't flat.",
-            image: {
-                url: "https://i.ytimg.com/vi/0wQ5bFRoJe0/maxresdefault.jpg"
-            },
-            fields: []
-            }
-       })
-    }
-
-    if(message.content.startsWith("!earthchan yes")){
-        lovemeter = lovemeter - 1
-        message.channel.send({embed: {
-            author: {
-                name: "Earth-Chan",
-                icon_url: "https://vignette.wikia.nocookie.net/virtualyoutuber/images/f/f2/EarthChan.jpg/revision/latest?cb=20180527004331"
-            },
-            description: "oh... Mood:" + lovemeter + "/5." ,
-            image: {
-                url: "https://images-cdn.9gag.com/photo/ad9mpwB_700b.jpg"
-            },
-            fields: []
-            }
-       })
-    }
-
-    if(message.content.startsWith("!earthchan no")){
-        lovemeter = lovemeter + 1
-        message.channel.send({embed: {
-            author: {
-                name: "Earth-Chan",
-                icon_url: "https://vignette.wikia.nocookie.net/virtualyoutuber/images/f/f2/EarthChan.jpg/revision/latest?cb=20180527004331"
-            },
-            description: "oh Thank you! Mood:" + lovemeter + "/5." ,
-            image: {
-                url: "https://pm1.narvii.com/6686/a280a434bbcef5a048345768a730f1a0258ccea2_hq.jpg"
-            },
-            fields: []
-            }
-       })
-    }
-
 
     //Love-Chan
+    if (message.content.startsWith("!lovekun help")) {
+        message.reply({embed: {
+            color: 15077906,
+            author: {
+                name: "Love-Kun",
+                icon_url: "https://pm1.narvii.com/6739/5589806a476247d5bd3052a4690bc19d176373cev2_hq.jpg"
+            },
+            description: 'Hey there you naughty. Here are my commands~ UwU <3',
+            fields: [
+                {
+                    name: '!lovekun ship',
+                    value: 'Ships 2 lucky users on the discord...',
+                },
+                {
+                    name: '!lovekun marriage',
+                    value: 'Ships 2 lucky users on the discord...',
+                },
+                {
+                    name: '!lovekun kink [BROKEN/WIP]',
+                    value: 'The comand explains itself love~',
+                },
+                {
+                    name: '!lovekun ethan',
+                    value: 'Ships with nobody. again. He dosent deserve love.',
+                },
+                {
+                    name: '!lovekun mercedes',
+                    value: 'Ya know, who knows. Maybe destiny will find you two together.',
+                },
+                {
+                    name: '!lovekun marissa',
+                    value: 'Should I say more?',
+                },
+                {
+                    name: '!lovekun daniel',
+                    value: 'Hes lucky that I even put him here.',
+                },
+                {
+                    name: 'MORE COMMANDS COMING SOON!',
+                    value: '(Please mention me if you want to be added.)',
+                },
+            ],
+        }
+        }); 
+    }
 
     if(message.content.startsWith("!lovekun ship")){
         message.channel.send({embed: {
@@ -172,6 +179,19 @@ bot.on('message', async message => {
                 icon_url: "https://pm1.narvii.com/6739/5589806a476247d5bd3052a4690bc19d176373cev2_hq.jpg"
             },
             description: vartom + " and " + varnom + " kissing in a tree. K I S S I N G~",
+            color: 15077906,
+            fields: []
+            }
+       })
+    }
+
+    if(message.content.startsWith("!lovekun marriage")){
+        message.channel.send({embed: {
+            author: {
+                name: "Love-Kun",
+                icon_url: "https://pm1.narvii.com/6739/5589806a476247d5bd3052a4690bc19d176373cev2_hq.jpg"
+            },
+            description: "I pronounce " + vartom + " and " + varhom + " husband and wife (you choose!) Wow congratulations to them.",
             color: 15077906,
             fields: []
             }
@@ -199,9 +219,6 @@ bot.on('message', async message => {
             },
             description: "Merecedes's perfect match is... " + vartom + ".",
             color: 15077906,
-            image: {
-                 "url": "https://pm1.narvii.com/6739/5589806a476247d5bd3052a4690bc19d176373cev2_hq.jpg"
-               },
             fields: []
             }
        })
@@ -215,70 +232,122 @@ bot.on('message', async message => {
             },
             description: "Marissa's pet is " + vartom + ".",
             color: 15077906,
-            image: {
-                 "url": "https://pm1.narvii.com/6739/5589806a476247d5bd3052a4690bc19d176373cev2_hq.jpg"
-               },
+            fields: []
+            }
+       })
+    }
+
+    if(message.content.startsWith("!lovekun daniel")){
+        message.channel.send({embed: {
+            author: {
+                name: "Love-Kun",
+                icon_url: "https://pm1.narvii.com/6739/5589806a476247d5bd3052a4690bc19d176373cev2_hq.jpg"
+            },
+            description: "Daniel felt weird... It's like he was falling in love with " + vartom + ".",
+            color: 15077906,
             fields: []
             }
        })
     }
 
     //Duolingo-Chan Stuff
-    if(message.content.startsWith("!openduolingo")){
+    if (message.content.startsWith("!duolingochan help")) {
+        message.reply({embed: {
+            color: 9043806,
+            author: {
+                name: "Duolingo-Chan",
+                icon_url: "https://i.ytimg.com/vi/qRE_yx_S5nY/maxresdefault.jpg"
+            },
+            description: 'Ruega por tu vida en Español',
+            fields: [
+                {
+                    name: '!duolingochan open [WIP]',
+                    value: 'Time for your spanish lessons...',
+                },
+                {
+                    name: 'MORE COMMANDS COMING SOON!',
+                    value: '.',
+                },
+            ],
+        }
+        }); 
+    }
+
+    if(message.content.startsWith("!duolingochan open")){
         if (message.author.id == Ethan){
             message.channel.send({embed: {
-                title: "Duolingo-Chan",
-                description: "Welcome to Duolingo Daniel!",
-                color: 44678,
-                image: {
-                     "url": "https://i.ytimg.com/vi/qRE_yx_S5nY/maxresdefault.jpg"
-                   },
+                author: {
+                    name: "Duolingo-Chan",
+                    icon_url: "https://i.ytimg.com/vi/qRE_yx_S5nY/maxresdefault.jpg"
+                },
+                description: "Hey Ethan. Sup.",
+                color: 9043806,
                 fields: []
                 }
            })
-        } else if (message.author.id == '408822490022805504'){
+        } else if (message.author.id == Mercedes){
             message.channel.send({embed: {
-                title: "Duolingo-Chan",
-                description: "Hello Olivia, welcome to the Duolingo app. I am currently being built by my master. Please wait until he has finished my core functions.",
-                color: 44678,
+                author: {
+                    name: "Duolingo-Chan",
+                    icon_url: "https://i.ytimg.com/vi/qRE_yx_S5nY/maxresdefault.jpg"
+                },
+                description: "Hey Mercedes. Wanna learn some korean you koreaboo.",
+                color: 9043806,
                 fields: []
                 }
-            })
-        } else if (message.author.id == '268519886328692737'){
+           })
+        } else if (message.author.id == Marissa){
             message.channel.send({embed: {
-                title: "Duolingo-Chan",
-                description: "Hello Marissa, welcome to the Duolingo app. I am currently being built by my master. Please wait until he has finished my core functions.",
-                color: 44678,
+                author: {
+                    name: "Duolingo-Chan",
+                    icon_url: "https://i.ytimg.com/vi/qRE_yx_S5nY/maxresdefault.jpg"
+                },
+                description: "Marissa... you missed an Italian lesson... You know what comes next.",
+                color: 9043806,
                 fields: []
                 }
-            })
-        } else if (message.author.id == '196593261828243456'){
+           })
+        } else if (message.author.id == Olivia){
             message.channel.send({embed: {
-                title: "Duolingo-Chan",
-                description: "Hello Mercedes, welcome to the Duolingo app. I am currently being built by my master. Please wait until he has finished my core functions.",
-                color: 44678,
+                author: {
+                    name: "Duolingo-Chan",
+                    icon_url: "https://i.ytimg.com/vi/qRE_yx_S5nY/maxresdefault.jpg"
+                },
+                description: "Next time you miss a lesson Olivia... You'll meet Ethan's closet next time.",
+                color: 9043806,
                 fields: []
                 }
-            })
-        } else if (message.author.id == '340630936972689408'){
+           })
+        } else if (message.author.id == Daniel){
             message.channel.send({embed: {
-                title: "Duolingo-Chan",
-                description: "Welcome to Duolingo Daniel!",
-                color: 44678,
-                image: {
-                     "url": "https://i.ytimg.com/vi/qRE_yx_S5nY/maxresdefault.jpg"
-                   },
+                author: {
+                    name: "Duolingo-Chan",
+                    icon_url: "https://i.ytimg.com/vi/qRE_yx_S5nY/maxresdefault.jpg"
+                },
+                description: "Daniel... hope you enjoy your note on the door...",
+                color: 9043806,
+                fields: []
+                }
+           })
+        } else if (message.author.id == Dylan){
+            message.channel.send({embed: {
+                author: {
+                    name: "Duolingo-Chan",
+                    icon_url: "https://i.ytimg.com/vi/qRE_yx_S5nY/maxresdefault.jpg"
+                },
+                description: "Dylan. Congrats! You passed your spanish class!",
+                color: 9043806,
                 fields: []
                 }
            })
         } else {
-            message.channel.send({embed: {
-                title: "Duolingo-Chan",
-                description: "Welcome to Duolingo Daniel!",
-                color: 44678,
-                image: {
-                     "url": "https://i.ytimg.com/vi/qRE_yx_S5nY/maxresdefault.jpg"
-                   },
+            message.reply({embed: {
+                author: {
+                    name: "Duolingo-Chan",
+                    icon_url: "https://i.ytimg.com/vi/qRE_yx_S5nY/maxresdefault.jpg"
+                },
+                description: "Hey, I noticed you're not signed up with Duolingo. Please contact Ethan if you'd like to join!",
+                color: 9043806,
                 fields: []
                 }
            })
@@ -286,26 +355,48 @@ bot.on('message', async message => {
     }
 
     //Google-Chan
+    if (message.content.startsWith("!chromechan help")) {
+        message.reply({embed: {
+            color: 15924992,
+            author: {
+                name: "Chrome-Chan",
+                icon_url: "https://i.imgur.com/yTGegZ7.png"
+            },
+            description: 'Lemme eat your ram.',
+            fields: [
+                {
+                    name: '!chromechan open [WIP]',
+                    value: 'Lets eat up all that tasty ram you have there!',
+                },
+                {
+                    name: 'MORE COMMANDS COMING SOON!',
+                    value: '.',
+                },
+            ],
+        }
+        }); 
+    }
+
     if(message.content.startsWith("!openchrome")){
-        if (message.author.id == '416429218960769026'){
+        if (message.author.id == Ethan){
             message.channel.send({embed: {
-                title: "Google-Chan",
-                description: "Thank you ethan for installing Google Chrome!",
-                color: 44678,
-                image: {
-                     "url": "https://i.imgur.com/yTGegZ7.png"
-                   },
+                author: {
+                    name: "Chrome-Chan",
+                    icon_url: "https://i.ytimg.com/vi/qRE_yx_S5nY/maxresdefault.jpg"
+                },
+                description: "Woo! Installed.",
+                color: 15924992,
                 fields: []
                 }
            })
         } else {
-            message.reply({embed: {
-                title: "Google-Chan",
-                description: "Hello new user! Welcome to Google!",
-                color: 44678,
-                image: {
-                     "url": "https://i.imgur.com/yTGegZ7.png"
-                   },
+            message.channel.send({embed: {
+                author: {
+                    name: "Chrome-Chan",
+                    icon_url: "https://i.ytimg.com/vi/qRE_yx_S5nY/maxresdefault.jpg"
+                },
+                description: "Sorry user! A͏̫n̴̬̘̭̟̬̞ è̻̖̙͎͓̥ṟ̭̙͍͘r͏̱ǫ̩͍̼̟̹̯̜r̯̪͈͖̘ ̸͉̬͔̠̯h̭̰͟ͅa̡͍͎̖̠̜͉s̺̰̙̬̻ ̡̖o҉̠̜͍͔c̳͖͈̗͕͟ͅͅc͖̺ṳ̹͟r̨͚̯͕e̛͚̲̼̪̝̦̮d͈̤̩̤̻̳̱.̠̻̥̦͜",
+                color: 15924992,
                 fields: []
                 }
            })
@@ -323,7 +414,7 @@ bot.on('message', async message => {
                },
             fields: []
             }
-       })
+       });
     }
 
 });     
