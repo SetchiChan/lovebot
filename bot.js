ad53ea3ba1061fa0d3a9d5a4134fb4c39ec9b477
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2019-12-17 00:10:20 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
+const Storage = require('discord-storage').Storage;
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setGame('This is SCP.')
+    client.user.setGame('This is SCP. 1')
 })
 
 client.login(process.env.BOT_TOKEN);
