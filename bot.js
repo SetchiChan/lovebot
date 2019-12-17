979e9f4e5630719611739a7b400cbd76a3b3f6a1
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2019-12-17 00:08:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,8 @@
 const Discord = require('discord.js');
-const Storage = require('discord-storage').Storage;
 const client = new Discord.Client();
 
-client.once('ready', () => {
-	client.storage = new Storage(client, '509890599093141516');
-	client.storage.sync().then(() => {
-        client.channels.get("509890599093141516").send("Hello mate!")
-	});
-}); 
-
 client.on('ready', () => {
-    bot.user.setGame('WIP? Mate?')
+    client.user.setGame('This is SCP.')
 })
 
 client.login(process.env.BOT_TOKEN);
