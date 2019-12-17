aea9e43f9700b56260683b574e3b69e82286d40b
SetchiChan <35151486+SetchiChan@users.noreply.github.com>
2019-12-16 23:40:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const Storage = require('discord-storage').Storage;
 const bot = new Discord.Client();
 
 const newUser = [];
@@ -7,23 +8,33 @@ const PREFIX = "'";
 const TOKEN = "x";
 
 bot.on('message', async message => {
+    const vartom = message.guild.members.random();
+    const varnom = message.guild.members.random();
+    const varhom = message.guild.members.random();
 
-    if (message.content.startsWith("tacobell")) {
-        message.channel.send('Yes?');
-    }    
-
-});    
+    if(message.content.startsWith("?lovekun mercedes")){
+        message.channel.send({embed: {
+            author: {
+                name: "Love-Kun",
+                icon_url: "https://pm1.narvii.com/6739/5589806a476247d5bd3052a4690bc19d176373cev2_hq.jpg"
+            },
+            description: "Merecedes's perfect match is... " + vartom + ".",
+            color: 15077906,
+            fields: []
+            }
+       })
+    }
+});     
 
 bot.once('ready', () => {
-	bot.storage = new Storage(bot, '537127370423926844');
-	bot.storage.sync().then(() => {
-		message.channel.send("Hello");
-	});
+    bot.storage = new Storage(bot, '509890599093141516');
+    bot.storage.sync().then(() => {
+        channels.get('509890599093141516').send('Im gonna evolve');
+    });
 });
 
 bot.on('ready', () => {
-    bot.user.setGame('do "?commandlist" for commands! Welcome to the universe!')
+    bot.user.setGame('WIP...')
 })
-
 
 bot.login(process.env.BOT_TOKEN);
